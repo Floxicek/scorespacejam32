bc10e3d8d1d48a6e4719caa9c0d62dd685eefb38
Floxicek <floxbox12@gmail.com>
2024-11-02 14:48:38 +0100

    Add camera zoom

---
+++ scripts/camera_controller.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+@export_category("Zoom")
+@export var zoom_increment := 0.2
+@export var zoom_speed := 0.5
+@export var zoom_min := 0.6
+@export var zoom_max := 1.5
+
+var zoom_target = 1.0
+
+func _process(delta):
+	var zoom_current = zoom
+	
+	if Input.is_action_just_released("zoom_in"):
+		zoom_target += zoom_increment
+	elif Input.is_action_just_released("zoom_out"):
+		zoom_target -= zoom_increment
+	zoom_target = clamp(zoom_target, zoom_min, zoom_max)
+	var z = lerp(zoom.x, zoom_target, zoom_increment * delta * 60 * zoom_speed)
+	zoom = Vector2(z,z)
