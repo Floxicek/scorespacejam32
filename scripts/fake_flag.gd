b6dc34fd22ac09089289e27df19f486cc281f97e
Daniel Martinek <bubbledan@seznam.cz>
2024-11-03 14:32:00 +0100

    Partial level1 with a surprise

---
+++ scripts/fake_flag.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		var tween = get_tree().create_tween()
+		tween.tween_property(%SikeText, "modulate:a", 1, 4).set_trans(Tween.TRANS_SINE)
